907eb71537bc2ff1fa79ea5d3d0d5223a55d67eb
Evaxtt <xuting.me@gmail.com>
2016-02-17 16:06:15 +0800

    first commit

---
+++ theme.py
@@ -0,0 +1,14 @@
+name = 'Theme'
+author = 'Evaxtt'
+homepage = 'https://github.com/Evaxtt/Theme.git'
+post_per_page = 100
+vars = {
+    "github": "Evaxtt",
+    "links": [
+        {
+            "name": "catsup",
+            "url": "https://github.com/whtsky/catsup",
+            "description": "An Blog System"
+        }
+    ]
+}
